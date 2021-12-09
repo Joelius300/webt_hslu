249595b348e2decc5a6914659f77c1f9cb7c9641
Joel Liechti <bstlng321@gmail.com>
2021-12-09 11:17:04 +0100

    Finish display of just added mushroom

--- mushrooms.php
+++ mushrooms.php
@@ -1,39 +1,40 @@
 <?php
 
-const VALID_COLORS = ['saddlebrown', 'lightgray', 'rosybrown', 'red', 'yellow'];
+const VALID_COLORS = ['saddlebrown' => 'Hellbraun', 'lightgray' => 'Hellgrau', 'rosybrown' => 'Rosenbraun', 'red' => 'Rot', 'yellow' => 'Gelb'];
 
 // Freitext
 if (empty($_POST['name'])) { // equivalent to !isset($var) || $var == false, according to php documentation
     echo "Name muss angegeben werden.";
+    return;
 }
 
 // Kontinuierlicher Wert
 if (!isset($_POST['height']) || !is_numeric($_POST['height']) || (int)$_POST['height'] <= 0) {
     echo "Grösse muss eine positive Ganzzahl sein.";
+    return;
 }
 
 // Diskreter Wert
-if (empty($_POST['color']) || !in_array($_POST['color'], VALID_COLORS, true)) {
+if (empty($_POST['color']) || !in_array($_POST['color'], array_keys(VALID_COLORS), true)) {
     echo "Es muss eine der angezeigten Farben ausgewählt werden.";
+    return;
 }
-// Die Client-Validierung, wo auf bekannte Pilze hingewiesen wird, ist nur eine Hilfestellung
-// ohne Folgen auf Sicherheit o.Ä., und kann auf dem Server vernachlässigt werden.
 
 $name = $_POST['name'];
 $height = $_POST['height'];
 $color = $_POST['color'];
-
-// TODO map the color to a german word to display
+$colorGerman = VALID_COLORS[$color];
 
 ?>
 
-<div class="w3-card">
-    <header class="w3-container">
+<div>
+    <header>
+        <h2>Pilz hinzugefügt</h2>
         <h3><?php echo $name ?></h3>
     </header>
-    <div class="w3-container">
+    <div>
         <p>Grösse: <?php echo $height ?></p>
-        <p>Farbe: <?php echo $color ?></p>
+        <p>Farbe: <?php echo $colorGerman ?></p>
     </div>
 </div>
 
